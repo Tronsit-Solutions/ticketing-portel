0a28911c199ae1e511815ea0c96db229554d6a82
Shehroz-94 <shehrozehsan@gmail.com>
2026-06-09 21:30:33 +0500

    refined catalogue layout and few forms

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -34,9 +34,10 @@ class TicketsController < ApplicationController
   end
 
   def new
-    @ticket      = Ticket.new(ticket_type: params[:ticket_type])
-    @ticket_type = Ticket::CATALOGUE.find { |c| c[:type] == params[:ticket_type] }
-    @customers   = User.customers.active.order(:fullname) if current_user.agent? || current_user.admin?
+    @ticket           = Ticket.new(ticket_type: params[:ticket_type])
+    @ticket_type      = Ticket::CATALOGUE.flat_map { |c| c[:children] || [c] }.find { |c| c[:type] == params[:ticket_type] }
+    @customers        = User.customers.active.order(:fullname) if current_user.agent? || current_user.admin?
+    @my_tickets_count = Ticket.where(customer: current_user).count if current_user.customer?
   end
 
   def create
@@ -74,7 +75,7 @@ class TicketsController < ApplicationController
       end
       redirect_to @ticket, notice: "Ticket submitted successfully."
     else
-      @ticket_type = Ticket::CATALOGUE.find { |c| c[:type] == params[:ticket][:ticket_type] }
+      @ticket_type = Ticket::CATALOGUE.flat_map { |c| c[:children] || [c] }.find { |c| c[:type] == params[:ticket][:ticket_type] }
       @customers   = User.customers.active.order(:fullname) if current_user.agent? || current_user.admin?
       render :new, status: :unprocessable_entity
     end
