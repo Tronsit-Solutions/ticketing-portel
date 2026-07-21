26defe4745bfc7d8b12855c7662b8d5dfcc57d70
Shehroz-94 <shehrozehsan@gmail.com>
2026-07-21 18:21:54 +0500

    added real time update for ticket messages

--- app/models/ticket_message.rb
+++ app/models/ticket_message.rb
@@ -11,4 +11,24 @@ class TicketMessage < ApplicationRecord
   scope :visible,        -> { where(internal_note: false) }
   scope :internal_notes, -> { where(internal_note: true) }
   scope :recent,         -> { order(created_at: :asc) }
+
+  after_create_commit :broadcast_created
+
+  private
+
+  HTML_TAG_PATTERN = /<\s*(p|br|div|span|ul|ol|li|strong|em|a|img|table|tr|td|h[1-6])\b/i
+
+  def broadcast_created
+    broadcast_append_later_to [ticket, :messages],
+      target: "ticket_messages_agent",
+      partial: "ticket_messages/message_agent",
+      locals: { message: self } unless details.match?(HTML_TAG_PATTERN)
+
+    return if internal_note?
+
+    broadcast_append_later_to [ticket, :messages],
+      target: "ticket_messages_customer",
+      partial: "ticket_messages/message_customer",
+      locals: { message: self }
+  end
 end
